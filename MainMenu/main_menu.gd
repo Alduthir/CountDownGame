6beb3ea646109c0ed8a0e9ed1f25ab276706e747
Timo Terpstra <teti@summacollege.nl>
2026-07-22 23:17:46 +0200

    Added the option to go to the settings page and added the code to start a game.

--- MainMenu/main_menu.gd
+++ MainMenu/main_menu.gd
@@ -5,10 +5,15 @@ extends Control
 func _ready() -> void:
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func _on_btn_start_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_btn_settings_pressed() -> void:
+	get_tree().change_scene_to_file("res://MainMenu/Settings/Settings.tscn") # Replace with function body.
 
 func _on_btn_exit_pressed() -> void:
 	get_tree().quit()
