53de6e81dd81ed1f908a4b32f8a26208f07afab2
Sander <husn@summacollege.nl>
2026-07-25 14:57:36 +0200

    spawnlocations

--- NPCs/Villagers/villager_manager.gd
+++ NPCs/Villagers/villager_manager.gd
@@ -6,11 +6,14 @@ extends Node2D
 @export var spawn_area_top_left := Vector2(-200, -120)
 @export var spawn_area_bottom_right := Vector2(200, 120)
 
-@export var spawn_locations : Array[Vector2]
+@export var spawn_locations: Array[Vector2] = []
+
 var villagers: Array[Node] = []
 
 
 func _ready() -> void:
+	randomize()
+
 	spawn_villagers(start_villagers)
 	print("VillagerManager gestart")
 
@@ -19,7 +22,7 @@ func spawn_villagers(amount: int) -> void:
 	if amount <= 0:
 		return
 
-	for i in amount:
+	for i in amount:ssss
 		spawn_villager()
 
 
@@ -30,16 +33,7 @@ func spawn_villager() -> void:
 
 	var villager := villager_scene.instantiate()
 
-	villager.position = Vector2(
-		randf_range(
-			spawn_area_top_left.x,
-			spawn_area_bottom_right.x
-		),
-		randf_range(
-			spawn_area_top_left.y,
-			spawn_area_bottom_right.y
-		)
-	)
+	villager.position = get_random_spawn_position()
 
 	add_child(villager)
 	villagers.append(villager)
@@ -50,6 +44,27 @@ func spawn_villager() -> void:
 
 	update_villager_count()
 	print("Villager gespawned op: ", villager.position)
+
+
+func get_random_spawn_position() -> Vector2:
+	if not spawn_locations.is_empty():
+		return spawn_locations.pick_random()
+
+	push_warning(
+		"VillagerManager: geen spawn_locations ingesteld. " +
+		"Het willekeurige spawngebied wordt gebruikt."
+	)
+
+	return Vector2(
+		randf_range(
+			spawn_area_top_left.x,
+			spawn_area_bottom_right.x
+		),
+		randf_range(
+			spawn_area_top_left.y,
+			spawn_area_bottom_right.y
+		)
+	)
 
 
 func add_villagers(amount: int) -> void:
